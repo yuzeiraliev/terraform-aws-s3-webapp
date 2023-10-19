5595b7df19e5c5c4ce146fbb993c893e0b2de4ed
yuzeiraliev <141018040+yuzeiraliev@users.noreply.github.com>
2023-10-19 13:25:17 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,10 +20,36 @@ resource "aws_s3_bucket_acl" "bucket" {
   bucket = aws_s3_bucket.bucket.id
 
   acl = "public-read"
+  #adding additional lines for proper ordering of resource creation
+  depends_on = [
+  aws_s3_bucket_ownership_controls.example,
+  aws_s3_bucket_public_access_block.example
+  ]
+}
+#adding additional blocks becuase of failed/outdated tutorial
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+#adding new blocks finishes here
 resource "aws_s3_bucket_policy" "policy" {
   bucket = aws_s3_bucket.bucket.id
+  #adding additional lines for proper ordering of resource creation
+  depends_on = [
+  aws_s3_bucket_ownership_controls.example,
+  aws_s3_bucket_public_access_block.example
+  ]
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -48,6 +74,11 @@ resource "aws_s3_object" "webapp" {
   acl          = "public-read"
   key          = "index.html"
   bucket       = aws_s3_bucket.bucket.id
+  #adding additional lines for proper ordering of resource creation
+  depends_on = [
+  aws_s3_bucket_ownership_controls.example,
+  aws_s3_bucket_public_access_block.example
+  ]
   content      = file("${path.module}/assets/index.html")
   content_type = "text/html"
 }
